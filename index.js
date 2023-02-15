516ad4c1fcf1b0e73f257f5fcd3821318550917e
Anaïs Hammouche <72383729+AnaisHammouche@users.noreply.github.com>
2023-02-15 16:02:54 +0100

    form + navigation + loginBind (#1)
    Co-authored-by: Anais <anais.hammouche@edu.itescia.fr>

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import LogInBind from './src/views/non-voyants/LoginBind';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => LogInBind);
