8e56d1ee7a3cfd8ce4f9d56c9791607de36d60f0
Anais <anais.hammouche@edu.itescia.fr>
2023-04-27 10:46:06 +0200

    bug match

--- src/views/non-voyants/formRouteBlind.js
+++ src/views/non-voyants/formRouteBlind.js
@@ -22,6 +22,7 @@ const FormRouteBlind = ({ route, navigation }) => {
   const routeParamsToken = route.params.token;
   const [fromStation, setfromStation] = useState();
   const [toStation, setToStation] = useState();
+  const [routeMateGender, setRouteMateGender] = useState();
   const [date, setDate] = useState();
   const [time, setTime] = useState();
 
@@ -34,44 +35,45 @@ const FormRouteBlind = ({ route, navigation }) => {
       .post("http://localhost:8080/api/v1/routes/add", {
         fromStation: fromStation,
         toStation: toStation,
+        routeMateGender: routeMateGender
 
 })
       .then(async function (response) {
-        const getAsynTokenStorage = await AsyncStorage.getItem('Token');
+      //   const getAsynTokenStorage = await AsyncStorage.getItem('Token');
         console.log("routeParamsToken async:" + JSON.parse(getAsynTokenStorage))
-        if (response.status = "200" && fromStation && toStation ) {
-          const fromStationData = JSON.stringify(fromStation);
-          console.log('fromstationPost : ' + fromStationData);
-          const toStationData = JSON.stringify(toStation);
-          console.log('tostationDataPost : ' + toStationData );        
-           return getAsynTokenStorage != null ? JSON.parse(getAsynTokenStorage) : null;
-         }
+      //   if (response.status = "200" && (fromStation && toStation)) {
+      //     const fromStationData = JSON.stringify(fromStation);
+      //     console.log('fromstationPost : ' + fromStationData);
+      //     const toStationData = JSON.stringify(toStation);
+      //     console.log('tostationDataPost : ' + toStationData );        
+      //      return getAsynTokenStorage != null ? JSON.parse(getAsynTokenStorage) : null;
+      //    }
 
       })
       
-      .catch(function (error) {
-        if (!fromStation || !toStation ) { 
-          console.log("champ vide")
-        alert('erreur : ' + JSON.stringify(error));
-        console.log("perdu ! " + error)
-      }});
+      // .catch(function (error) {
+      //   if (!fromStation || !toStation ) { 
+      //     console.log("champs vide")
+      //   alert('erreur : ' + JSON.stringify(error));
+      //   console.log("perdu ! " + error)
+      // }});
 
-      axios.get("http://localhost:8080/api/v1/routes/matches", {
-        fromStation: fromStation,
-        toStation: toStation,
-      })
-      .then(function (response){
-        if (response.data){
-          navigation.navigate('Waiting')
-          console.log("dans response data get")
-          axios.post('http://localhost:8080/api/v1/sendgrid', {
+      // axios.get("http://localhost:8080/api/v1/routes/matches", {
+      //   fromStation: fromStation,
+      //   toStation: toStation,
+      // })
+      // .then(function (response){
+      //   if (response.data){
+      //     navigation.navigate('Waiting')
+      //     console.log("dans response data get")
+      //     axios.post('http://localhost:8080/api/v1/sendgrid', {
 
-          })
-          .then(async function (response){
-            console.log('dans le post sendgrid')
-            })
+      //     })
+      //     .then(async function (response){
+      //       console.log('dans le post sendgrid')
+      //       })
            
-          }}
+      //     }}
           
           // const getAsynTokenStorage = AsyncStorage.getItem('Token');
           //  const fromStationData = fromStation;
@@ -87,15 +89,15 @@ const FormRouteBlind = ({ route, navigation }) => {
           //  console.log(response.data[0]['fromStation'] + response.data[0]['fromStation'] )
         
       
-      .catch(function (error) {
-        if ( !response.data || response.data == null ) {  
-          console.log("get erreur reponse vide ou null !") 
-  } else {
-    console.log("get erreur !")
-    alert('erreur : ' + JSON.stringify(error));
-    console.log("perdu ! " + error)
-  }
-}))
+//       .catch(function (error) {
+//         if ( !response.data || response.data == null ) {  
+//           console.log("get erreur reponse vide ou null !") 
+//   } else {
+//     console.log("get erreur !")
+//     alert('erreur : ' + JSON.stringify(error));
+//     console.log("perdu ! " + error)
+//   }
+// }))
 
 }, []);
 
