fd8d9cdbd7353c095b424ed806918f6bd9460ccb
Flora <flora.julie88@gmail.com>
2023-05-12 11:54:44 +0200

    add return await in axiosRegister

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -12,7 +12,7 @@ export async function axiosRegister(
   isBlind,
   navigation,
 ) {
-  axios
+  return await axios
     .post(`${baseUrl}/auth/register`, {
       lastname: lastName,
       firstname: firstName,
