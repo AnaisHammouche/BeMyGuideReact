ad6d808143f0807b010da0a147479596bc3f80f7
Claire <claire@MacBook-Pro-de-coding-7.local>
2023-05-22 17:39:38 +0200

    tabBarBlind with labels

--- src/components/navigators/BottomTabNavigator.js
+++ src/components/navigators/BottomTabNavigator.js
@@ -8,43 +8,9 @@ import ProfileScreen from '../../views/profile';
 import {styles} from '../../styles/register_style';
 
 const Tab = createBottomTabNavigator();
-// const isBlind = JSON.parse(route.params.userIsBlind);
-// console.log('isBlindTAB : ' + isBlind)
 
 const BottomTabNavigator = ({navigation, route}) => {
   const isBlind = JSON.parse(route.params.userIsBlind);
-
- /*  function Item(userIsBlind) {
-    userIsBlind = isBlind;
-    if (!userIsBlind) {
-      console.log('inside function' + userIsBlind);
-      return (
-        
-        <Tab.Screen
-        name="Rewards"
-        component={Rewards}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/reward.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-            </View>
-          ),
-        }}
-      />
-      )
-    } else {
-      return null;
-    }
-  } */
 
   return (
     <Tab.Navigator
@@ -59,182 +25,196 @@ const BottomTabNavigator = ({navigation, route}) => {
         },
         headerShown: false,
       }}>
-        {!isBlind ? (
-          <>
-      <Tab.Screen
-        name="Route"
-        component={FormRouteBlind}
-        initialParams={{isBlindUser: isBlind}}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/nav.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-               <Text style={styles.text}>Recherche</Text>
-       
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Trajets"
-        component={DisplayAllMyRoutesBlind}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/close_eye.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-               <Text style={styles.text}>Trajets</Text>
-       
-            </View>
-          ),
-        }}
-      />
-      {/* <Item userIsBlind={true} /> */}
-
-      <Tab.Screen
-        name="Rewards"
-        component={Rewards}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/reward.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-               <Text style={styles.text}>Rewards</Text>
-       
-            </View>
-          ),
-        }}
-      />
-
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          // tabBarLabel: ({focused}) => (   <Text  style={{
-          //   width: 25,
-          //   height: 25,
-          //   tintColor: focused ? '#27AE60' : '#748c94',
-          // }} >Profil</Text>
-          // ),
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/profil.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-               <Text >Profil</Text>
-       
-            </View>
-          ),
-        }}
-      />
-</>
-        ) : (
-          <>
+      {!isBlind ? (
+        <>
           <Tab.Screen
-        name="Route"
-        component={FormRouteBlind}
-        initialParams={{isBlindUser: isBlind}}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/nav.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-               <Text style={styles.text}>Recherche</Text>
-       
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Trajets"
-        component={DisplayAllMyRoutesBlind}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/close_eye.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-            </View>
-          ),
-        }}
-      />
-      {/* <Item userIsBlind={true} /> */}
+            name="Route"
+            component={FormRouteBlind}
+            initialParams={{isBlindUser: isBlind}}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/nav.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text style={styles.text}>Recherche</Text>
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Trajets"
+            component={DisplayAllMyRoutesBlind}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/close_eye.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text style={styles.text}>Trajets</Text>
+                </View>
+              ),
+            }}
+          />
 
-     
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <View
-              style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-              <Image
-                source={require('../../assets/profil.png')}
-                resizeMode="contain"
-                style={{
-                  width: 25,
-                  height: 25,
-                  tintColor: focused ? '#27AE60' : '#748c94',
-                }}
-              />
-            </View>
-          ),
-        }}
-      />
-          
-          </>)}
-      {/* <Tab.Screen name="FormRouteBlind" component={FormRouteBlind} initialParams={{isBlindUser: isBlind}}/>
-     <Tab.Screen name="Routes" component={DisplayAllMyRoutesBlind} /> */}
+          <Tab.Screen
+            name="Rewards"
+            component={Rewards}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/reward.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text style={styles.text}>Rewards</Text>
+                </View>
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/profil.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text>Profil</Text>
+                </View>
+              ),
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Tab.Screen
+            name="Route"
+            component={FormRouteBlind}
+            initialParams={{isBlindUser: isBlind}}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/nav.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text style={styles.text}>Recherche</Text>
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Trajets"
+            component={DisplayAllMyRoutesBlind}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/close_eye.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text>Trajets</Text>
+                </View>
+              ),
+            }}
+          />
+          {/* <Item userIsBlind={true} /> */}
+
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarIcon: ({focused}) => (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    top: 10,
+                  }}>
+                  <Image
+                    source={require('../../assets/profil.png')}
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      tintColor: focused ? '#27AE60' : '#748c94',
+                    }}
+                  />
+                  <Text>Profil</Text>
+                </View>
+              ),
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
-       
   );
-}
+};
 
 export default BottomTabNavigator;
